8ee8fb86c3b81ba9c02a28dd360e3a0817e0cafa
hamdan-27 <hamdan102703@gmail.com>
2023-05-18 16:20:17 +0400

    api key shifted to toml

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ df = pd.read_csv('processed/embeddings.csv', index_col=0)
 df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
 
 # ViewIt OpenAI API key
-openai.api_key = st.secrets(['api_key'])
+openai.api_key = st.secrets['api_key']
 # openai.api_key = api_key
 
 def create_context(question, df, maxlen=1800, size="ada"):
