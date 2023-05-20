cbcd9cf15d714426b2427952f024d7573a08de3a
hamdan-27 <hamdan102703@gmail.com>
2023-05-20 15:05:04 +0400

    .

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ df = pd.read_csv('processed/embeddings.csv', index_col=0)
 df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
 
 # ViewIt OpenAI API key
-# openai.api_key = "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"
 openai.api_key = st.secrets['api_key']
 # openai.api_key = api_key
 
