9850259626cc0232c006e946a65863280f17df6e
hamdan-27 <hamdan102703@gmail.com>
2023-05-30 14:51:45 +0400

    changed avatars

--- main.py
+++ main.py
@@ -59,5 +59,5 @@ if user_input:
 
 if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state['past'][i], is_user=True, key=str(i)+'_user')
+        message(st.session_state['past'][i], is_user=True, avatar_style='thumbs', key=str(i)+'_user')
         message(st.session_state['generated'][i], key=str(i))
