e450e837e630ee7b8fff229e3a49e28285d49517
hamdan-27 <hamdan102703@gmail.com>
2023-05-31 18:39:21 +0400

    added data option, spinner, and caption

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import streamlit as st
-st.set_page_config(page_title="Viewit Property Analyst", page_icon="📊", 
+st.set_page_config(page_title="Viewit Property Analyst", page_icon="📊",
                    layout="centered", initial_sidebar_state="auto")
-
-import openai
+from model import get_answer, df_prefix
 from streamlit_chat import message
-from model import load_data, get_answer
+import openai
 
+col1, col2, col3 = st.columns(3)
 
-st.image("https://i.postimg.cc/Nfz5nZ8G/Logo.png", width=200)
+with col2:
+    st.image("https://i.postimg.cc/Nfz5nZ8G/Logo.png", width=200)
 
 # # Clear Session State Variables
 # for key in st.session_state.keys():
 #     del st.session_state[key]
 
 
-# df = load_old_data()
-df = load_data()
-
 # ViewIt OpenAI API key
 openai.organization = st.secrets['org']
 openai.api_key = st.secrets['api_key']
+
 
 if 'user_input' not in st.session_state:
     st.session_state['user_input'] = ''
@@ -34,20 +33,33 @@ def clear():
 st.title('ViewIt Chatbot 0.3')
 
 
+data_option = st.radio('Choose the data', ['pfraw.csv', 'real_estate1.csv'], 
+                       key='radio_option', horizontal=True)
+
+if data_option == 'pfraw.csv':
+    df, PREFIX = df_prefix('pfraw.csv')
+else:
+    df, PREFIX = df_prefix('real_estate1.csv')
+
+
 with st.expander("See the data being used"):
     st.write("Here's a sample of our transaction data")
     st.write(f"Total rows: {len(df)}")
     st.dataframe(df.head(10))
 
+
 # App Sidebar
 with st.sidebar:
     st.markdown(f"""
                 # About
-                This is version 0.3 of the Chatbot Assistant that will help you look for your desired properties.
+                This is version 0.3 of the Chatbot Assistant that will help you
+                  look for your desired properties.
                 
                 # How to use
-                Simply enter your query in the text field and the assistant will help you out.
+                Simply enter your query in the text field and the assistant 
+                will help you out.
                 """)
+
 
 st.text_input("Ask a question: ", key='widget',
               placeholder='Ask a question...', on_change=clear)
@@ -63,13 +75,23 @@ user_input = st.session_state.user_input
 
 # Generate a response if input exists
 if user_input:
-    output = str(get_answer(user_input, df, temperature=.2))
+    with st.spinner('Thinking...'):
+        output = str(get_answer(question=user_input, prompt_prefix=PREFIX, df=df))
 
-    # store chat
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
+        # store chat
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(output)
 
 if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state['past'][i], is_user=True, avatar_style='thumbs', key=str(i)+'_user')
+        message(st.session_state['past'][i], is_user=True,
+                avatar_style='thumbs', key=str(i)+'_user')
         message(st.session_state['generated'][i], key=str(i))
+
+
+st.write("---")
+st.caption(
+    """Made by Hamdan Mohammad. [GitHub](https://github.com/hamdan-27) | 
+    [Instagram](https://instagram/hxm.dxn_)""")
+# st.caption(
+#     "Like what we're building? [Join the waitlist](https://tally.so/r/w4QW4r)")
