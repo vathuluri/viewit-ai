02b34481bd2da348c4bed2f25527d6be3fed6b8f
hamdan-27 <hamdan102703@gmail.com>
2023-06-03 17:14:28 +0400

    load data changes

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-import openai
 import pandas as pd
 import streamlit as st
 from langchain.agents import create_pandas_dataframe_agent
@@ -7,37 +6,38 @@ from prompts import *
 
 icons = "😎,😶‍🌫️,🤯,👾,🤖,👽,🦾,🕵️,🤹,✨,👓,🕶️,🔑,🗝️,🩻,📽️,📷,🔍,🔎,💡,📈,📊,📍,📎,🌏,🏙️,🏡,🏠,🏢,🏬,🌇".split(',')
 
-# ViewIt OpenAI API key
-openai.organization = st.secrets['org']
-openai.api_key = st.secrets['api_key']
-
-
 @st.cache_data
-def load_data(filename='pfraw.csv'):
+def load_data(filename) -> pd.DataFrame:
     df = pd.read_csv(f"data/{filename}")
+    if 'Record Date' in df.columns:
+        df['Record Date'] = pd.to_datetime(df['Record Date'])
+    elif 'Date' in df.columns:
+        df['Date'] = pd.to_datetime(df['Date'])
     return df
 
 
 @st.cache_resource
-def load_agent(df, temperature, prompt_prefix=SAMPLE_PROMPT_PREFIX):
-    '''Loads the langchain datagrame agent for the specified dataframe.'''
+def load_agent(df, temperature, prompt_prefix=SAMPLE_PROMPT_PREFIX, model='text-davinci-003'):
+    '''Loads the langchain dataframe agent for the specified dataframe.'''
 
-    llm = OpenAI(temperature=0.2, model_name='text-davinci-003', openai_api_key=st.secrets['api_key'])
+    llm = OpenAI(temperature=temperature, model_name=model)
     agent = create_pandas_dataframe_agent(llm=llm, df=df, prefix=prompt_prefix)
     return agent
 
 
-def get_answer(question, prompt_prefix, df, temperature=0.2):
+def get_answer(question, prompt_prefix, df, model='text-davinci-003', temperature=0.2):
     agent = load_agent(df=df, temperature=temperature,
-                       prompt_prefix=prompt_prefix)
+                       prompt_prefix=prompt_prefix, model=model)
     response = agent.run(question)
     return response
 
 
 prefix_mapping = {
     'pfraw.csv': SAMPLE_PROMPT_PREFIX,
-    'real_estate1.csv': PROMPT_PREFIX
+    'real_estate1.csv': PROMPT_PREFIX,
+    'new_reidin_data.csv': REIDIN_PREFIX
 }
+
 
 @st.cache_data
 def df_prefix(filename):
