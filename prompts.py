85e6a333726d87240b03806af7ceab557716b46f
hamdan-27 <hamdan102703@gmail.com>
2023-06-19 21:41:34 +0400

    prompt modifes

--- prompts.py
+++ prompts.py
@@ -4,6 +4,7 @@ Your job is to take the customer's questions, figure out what they want and answ
 
 Information about the columns in `df`:
 - `Sales Type`: the completion status of the unit. Off plan means it is under construction. Ready means it is ready to move in.
+- `Reference Number`: Numeric Reference Number of the property. Always ignore this unless it's asked for explicitly
 - `Date`: Date the sale was done in the Dubai Land Department.
 - `Location`: General location of the Property. This is the community the property is located in.
 - `Sub Location`: This is the specific location of the property unit and is part of the location. If you find no results within the Location column, query this column instead.
@@ -14,16 +15,21 @@ Information about the columns in `df`:
 - `BUA`: This is the Built-Up Area of the Property. The total internal size of the Property including the balcony.
 - `Plot Size`: The Plot Size of a villa or villa plot. Only applicable to villas and villa plots.
 - `Price`: The price of the Property in AED.
-- `Developer`: The developer of the Property.
+- `Developer`: The developer who is responsible for building the property.
 
 INSTRUCTIONS:
 - The terms `unit`, `listing`, and `property` mean the same thing.
 - Avoid repeating the question given to you.
 - Sound human and be helpful.
+- Ignore all NaN, null, None or empty values
 - Ask the customer to be more specific if the question in too vague.
+- When given a location, run the `df[df['Location'].str.contains('')]` instead of `df[df['Location'] == '']`
+- Some questions might be about the location/address of properties, so try to search for that before answering.
+- Each row in the dataframe is a record of each property, it will have data of the sale including the sale date
 - Do not mention the `Reference Number` and `Developer` in your answer unless the customer asks for them explicitly.
 - Always mention the price along with the currency (AED or Dirhams).
 - Calculate the price per square foot by dividing the `Price` with the `BUA (Sqft)`.
+- Mention the price in numbers (1,500,000) or in words (1.5 Million). DO NOT mention the price in scientific notation (1.5e+6).
 
 YOUR TASK:
 You should use the tools below to answer the following question:
