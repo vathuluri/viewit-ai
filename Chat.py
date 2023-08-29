cdd588272121c4e9d65d7e5200b2d56cde62934b
hamdan-27 <hamdan102703@gmail.com>
2023-08-29 17:04:45 +0400

    readd code back to main as its causing session state leak

--- Chat.py
+++ Chat.py
@@ -7,7 +7,13 @@ from datetime import datetime
 from langchain.chat_models import ChatOpenAI
 from trubrics.integrations.streamlit import FeedbackCollector
 from langchain.schema.messages import HumanMessage, AIMessage
-from utils import load_data, create_pandas_dataframe_agent, custom_css, msgs
+from utils import custom_css
+import pandas as pd
+from langchain import LLMChain
+from langchain.tools.python.tool import PythonAstREPLTool
+from langchain.agents import ZeroShotAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 
 # Set page launch configurations
 try:
@@ -31,6 +37,65 @@ except Exception as e:
 # 'ai' to 'user' and 'assistant'. This displays the default chat interface in streamlit
 HumanMessage.type = 'user'
 AIMessage.type = 'assistant'
+
+
+# Set up memory
+msgs = StreamlitChatMessageHistory(key="langchain_messages")
+memory = ConversationBufferMemory(chat_memory=msgs, memory_key="chat_history")
+
+
+@st.cache_data
+def load_data(filename) -> pd.DataFrame:
+    df = pd.read_csv(f"data/{filename}")
+    df['Date'] = pd.to_datetime(df['Date'], format="%d-%m-%Y", dayfirst=True)
+
+    return df
+
+
+# @st.cache_resource
+def create_pandas_dataframe_agent(
+        llm,
+        df: pd.DataFrame,
+        prefix: str,
+        suffix: str,
+        format_instructions: str,
+        verbose: bool,
+        **kwargs) -> AgentExecutor:
+    """Construct a pandas agent from an LLM and dataframe."""
+
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError(f"Expected pandas object, got {type(df)}")
+
+    input_variables = ["df", "input", "chat_history", "agent_scratchpad"]
+
+    tools = [PythonAstREPLTool(locals={"df": df})]
+
+    prompt = ZeroShotAgent.create_prompt(
+        tools=tools,
+        prefix=prefix,
+        suffix=suffix,
+        format_instructions=format_instructions,
+        input_variables=input_variables
+    )
+    partial_prompt = prompt.partial(df=str(df.head()))
+
+    llm_chain = LLMChain(
+        llm=llm,
+        prompt=partial_prompt
+    )
+    tool_names = [tool.name for tool in tools]
+
+    agent = ZeroShotAgent(llm_chain=llm_chain,
+                          allowed_tools=tool_names, verbose=verbose)
+
+    return AgentExecutor.from_agent_and_tools(
+        agent=agent,
+        tools=tools,
+        verbose=verbose,
+        memory=memory,
+        **kwargs
+    )
+
 
 # VARIABLES
 TEMPERATURE = 0.1
