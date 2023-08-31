cddf9db1a91924761e6545e24dfc87292bf4886f
hamdan-27 <hamdan102703@gmail.com>
2023-08-31 21:22:51 +0400

    change project to default

--- Chat.py
+++ Chat.py
@@ -38,7 +38,7 @@ except Exception as e:
 def init_trubrics():
     """Initialize Trubrics FeedbackCollector"""
     collector = FeedbackCollector(
-        project="viewit-ae",  # TODO: change to viewit-ae before deployment
+        project="default",  # TODO: change to viewit-ae before deployment
         email=st.secrets.TRUBRICS_EMAIL,
         password=st.secrets.TRUBRICS_PASSWORD
     )
