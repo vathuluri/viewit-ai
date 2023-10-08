9b5d171ecd80395b7bed9c4bdbb54c8e049f0ea8
hamdan-27 <hamdan102703@gmail.com>
2023-10-08 13:56:09 +0400

    refactoring changes

--- pages/Feedback.py
+++ pages/Feedback.py
@@ -1,32 +1,24 @@
 import streamlit as st
-from utils import icon_style, hide_elements
+
+from utils.css import icon_style, hide_elements
 from Chat import collector, model
-# from trubrics.integrations.streamlit import FeedbackCollector
 
 try:
     st.set_page_config(page_title='Feedback • ViewIt.AI',
                        page_icon='📝', layout='wide')
 except:
-    st.experimental_rerun()
-
-# collector = FeedbackCollector(
-#     component_name="general feedback",
-#     # Store your Trubrics credentials in st.secrets:
-#     email=st.secrets["TRUBRICS_EMAIL"],
-#     # https://blog.streamlit.io/secrets-in-sharing-apps/
-#     password=st.secrets["TRUBRICS_PASSWORD"],
-# )
+    st.rerun()
 
 # Add Viewit logo image to the center of page
 col1, col2, col3 = st.columns([1,1.2,1])
 with col2:
     # st.image("https://i.postimg.cc/Nfz5nZ8G/Logo.png", width=300)
     st.image("imgs/Viewit ai Logo.png", width=300)
-
-
     st.subheader('⭐ Rate your experience!')
 
 st.write('---')
+
+# The feedback component
 feed = collector.st_feedback(
             component="general-feedback",
             feedback_type="textbox",
@@ -37,8 +29,11 @@ feed = collector.st_feedback(
             align="center",
         )
 
+# Sidebar
 with st.sidebar:
     # st.write("---")
+
+    # Social Icons
     st.write(f'''
     <div class="social-icons">
         <a href="https://viewit.ae" class="icon viewit" aria-label="ViewIt"></a>
